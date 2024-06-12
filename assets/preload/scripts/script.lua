4ef31b002f8ad5db42b08dc0bfbf46cbb13d4946
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2024-06-12 14:16:36 -0300

    Add files via upload

--- assets/preload/scripts/script.lua
+++ assets/preload/scripts/script.lua
@@ -1,5 +1,6 @@
 function onCreate() 
 makeLuaText('watermark',"Port By Mateuzinho ", 0, 2, 60); -- You can edit the created by Prisma Text just dont change anything else
+    setObjectCamera('watermark', 'camOther')
     setTextSize('watermark', 30);
     addLuaText('watermark');
 end
